c0e6b05637c4f4e5c95d3d789066ad23fe20b02a
K1dou <hique1276@gmail.com>
2024-02-27 21:50:52 -0300

    Order, Instant, ISO 8601, PART 2

--- src/main/java/com/educandoweb/curso/entities/Order.java
+++ src/main/java/com/educandoweb/curso/entities/Order.java
@@ -1,8 +1,11 @@
 package com.educandoweb.curso.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.persistence.Id;
+
+import javax.annotation.processing.Generated;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
@@ -19,21 +22,22 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-
-    public Order() {
-    }
     public Order(Long id, Instant moment, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
     }
 
+    public Order() {
+    }
 
     public Long getId() {
         return id;
